17761aefb0a7ab54b297e34dae2d3e1871e5d857
Robin claes <robinclaes1997@yahoo.fr>
2020-12-18 20:32:26 +0100

    Robin Unit test 18-12-2020 Fin test unitaire mapper

---
+++ UnitTest/TestMappeur/TestMapHealthpoint.cs
@@ -0,0 +1,60 @@
+﻿using API;
+using AutoMapper;
+using NUnit.Framework;
+using ProjetctTiGr13.Domain.FicheComponent;
+
+namespace UnitTest.TestMappeur
+{
+    [TestFixture]
+    public class TestMapHealthpoint
+    {
+        private IMapper _mapper;
+        private HealthPointManager _healthPointManagerDomain;
+        private Models.FicheModel.HealthPointManager _HealthPointManager;
+
+
+
+
+        [SetUp]
+        public void setUp()
+        {
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            
+            _mapper = mapperConfiguration.CreateMapper();
+            
+
+            _healthPointManagerDomain = new HealthPointManager();
+            _HealthPointManager = new Models.FicheModel.HealthPointManager()
+            {
+               PvActuel = 0,PvMax = 0,PvTemporaire = 0,DesDeVie = 0
+            };
+        }
+            [Test]
+            public void map_HealthPointManagerDomain_returnHealthPointManager()
+            {
+                var result = _mapper.Map<Models.FicheModel.HealthPointManager>(_healthPointManagerDomain);
+
+                Assert.AreEqual(_healthPointManagerDomain.CurrentHp, result.PvActuel);
+                Assert.AreEqual(_healthPointManagerDomain.MaxHp, result.PvMax);
+                Assert.AreEqual(_healthPointManagerDomain.TemporaryHp, result.PvTemporaire);
+                Assert.AreEqual(_healthPointManagerDomain.HpDice, result.DesDeVie);
+               
+            }
+
+            [Test]
+            public void map_HealthPointManager_returnHealthPointManagerDomain()
+            {
+                var result = _mapper.Map<HealthPointManager>(_HealthPointManager);
+
+                Assert.AreEqual(_HealthPointManager.PvActuel, result.CurrentHp);
+                Assert.AreEqual(_HealthPointManager.PvMax, result.MaxHp);
+                Assert.AreEqual(_HealthPointManager.PvTemporaire, result.TemporaryHp);
+                Assert.AreEqual(_HealthPointManager.DesDeVie, result.HpDice);
+              
+            }
+        
+    }
+}
---
+++ UnitTest/TestMappeur/testMapCompetence.cs
@@ -0,0 +1,52 @@
+﻿using API;
+using AutoMapper;
+using Models.FicheModel;
+using NUnit.Framework;
+using ProjetctTiGr13.Domain.FicheComponent;
+using Competence = ProjetctTiGr13.Domain.FicheComponent.Competence;
+
+namespace UnitTest.TestMappeur
+{
+    [TestFixture]
+    public class testMapCompetence
+    {
+        private IMapper _mapper;
+        private Competence _CompetenceDomain;
+        private Models.FicheModel.CompetencesFiche _Competence;
+
+        [SetUp]
+        public void setUp()
+        {
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            _mapper = mapperConfiguration.CreateMapper();
+
+            _CompetenceDomain = new Competence();
+            _Competence = new Models.FicheModel.CompetencesFiche()
+            {
+                IdComp = 0
+            };
+        }
+
+        [Test]
+        public void map_CompetenceDomain_returnCompetence()
+        {
+            var result = _mapper.Map<Models.FicheModel.CompetencesFiche>(_CompetenceDomain);
+
+            Assert.AreEqual(_CompetenceDomain.IdComp, result.IdComp);
+
+        }
+
+        [Test]
+        public void map_Competence_returnCompetenceDomain()
+        {
+            var result = _mapper.Map<Competence>(_Competence);
+
+            Assert.AreEqual(_Competence.IdComp, result.IdComp);
+
+        }
+
+    }
+}
